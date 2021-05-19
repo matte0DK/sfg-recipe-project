c3a0fd18a9ddbf01f83bd70716d7a8189b850e41
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-19 19:36:23 +0200

    added recipe and notes entity

---
+++ src/main/java/matteo/springframework/sfgrecipeproject/model/Notes.java
@@ -0,0 +1,36 @@
+package matteo.springframework.sfgrecipeproject.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
