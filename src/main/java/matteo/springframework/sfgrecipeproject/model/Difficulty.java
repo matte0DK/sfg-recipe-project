c512eeda24476ea8168e43313efd866f0cfeedb0
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-05-19 19:54:19 +0200

    created enum for difficultys and added enumarated annotation in recipe entity

---
+++ src/main/java/matteo/springframework/sfgrecipeproject/model/Difficulty.java
@@ -0,0 +1,8 @@
+package matteo.springframework.sfgrecipeproject.model;
+
+public enum Difficulty {
+
+    EASY,
+    MODERATE,
+    HARD
+}
--- src/main/java/matteo/springframework/sfgrecipeproject/model/Recipe.java
+++ src/main/java/matteo/springframework/sfgrecipeproject/model/Recipe.java
@@ -18,11 +18,12 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo add
-    // private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -32,6 +33,22 @@ public class Recipe {
 
     public Long getId() {
         return id;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public String getDescription() {
