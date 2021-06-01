b6c631f9512105b20892381da2c76bc6f5f3283b
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-06-01 23:13:23 +0200

    fixed recipeform.html and added methods to recipecontroller

--- src/main/java/matteo/springframework/sfgrecipeproject/controllers/RecipeController.java
+++ src/main/java/matteo/springframework/sfgrecipeproject/controllers/RecipeController.java
@@ -2,10 +2,14 @@ package matteo.springframework.sfgrecipeproject.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import matteo.springframework.sfgrecipeproject.commands.RecipeCommand;
+import matteo.springframework.sfgrecipeproject.model.Recipe;
 import matteo.springframework.sfgrecipeproject.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -19,5 +23,18 @@ public class RecipeController {
     public String showRecipeById(@PathVariable String id, Model model) {
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand recipeCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + recipeCommand.getId();
     }
 }
