4a15b59cff9379ae227f2560f897b5f9b5c490d4
matte0DK <72343291+matte0DK@users.noreply.github.com>
2021-06-03 17:22:40 +0200

    added functionality to persist an image to the database

--- src/main/java/matteo/springframework/sfgrecipeproject/service/ImageServiceImpl.java
+++ src/main/java/matteo/springframework/sfgrecipeproject/service/ImageServiceImpl.java
@@ -1,14 +1,40 @@
 package matteo.springframework.sfgrecipeproject.service;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import matteo.springframework.sfgrecipeproject.model.Recipe;
+import matteo.springframework.sfgrecipeproject.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@AllArgsConstructor
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService{
+    private final RecipeRepository recipeRepository;
+
     @Override
     public void saveImageFile(Long id, MultipartFile file) {
-        log.debug("received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(id).get();
+            Byte[] imageBytes = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b :
+                    file.getBytes()) {
+                imageBytes[i++] = b;
+            }
+
+            recipe.setImage(imageBytes);
+
+            recipeRepository.save(recipe);
+
+        } catch (IOException ioException) {
+            log.error("Error occured", ioException);
+            ioException.printStackTrace();
+        }
     }
 }
